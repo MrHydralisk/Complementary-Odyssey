0f62b06db307c869cd5e93d90c6d27576c06db9c
MrHydralisk <113328125+MrHydralisk@users.noreply.github.com>
2025-08-04 21:11:37 +0300

    Added custom VacDoor class. Changed hit points of VacDoor.

---
+++ Source/Odyssey Compact/Building_VacDoor.cs
@@ -0,0 +1,54 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+using System.Reflection;
+using RimWorld;
+using Verse.Sound;
+using UnityEngine;
+using Verse.AI;
+
+namespace OdysseyCompact
+{
+    public class Building_VacDoor : Building_SupportedDoor
+    {
+
+        private CompPowerTrader PowerTrader => powerTraderCached ?? (powerTraderCached = GetComp<CompPowerTrader>());
+        private CompPowerTrader powerTraderCached;
+
+        private VacuumComponent Vacuum => vacuumCached ?? (vacuumCached = base.MapHeld?.GetComponent<VacuumComponent>());
+        private VacuumComponent vacuumCached;
+
+        public override bool ExchangeVacuum => !IsAirtight || (Open && !PowerTrader.PowerOn);
+
+        protected override float TempEqualizeRate
+        {
+            get
+            {
+                if (!PowerTrader.PowerOn)
+                {
+                    return base.TempEqualizeRate;
+                }
+                return 0f;
+            }
+        }
+
+        protected override void ReceiveCompSignal(string signal)
+        {
+            if (signal == "PowerTurnedOn" || signal == "PowerTurnedOff")
+            {
+                Vacuum?.Dirty();
+            }
+        }
+
+        protected override void DrawAt(Vector3 drawLoc, bool flip = false)
+        {
+            base.DrawAt(drawLoc, flip);
+            if (PowerTrader.PowerOn && base.Map.Biome.inVacuum)
+            {
+                Graphic.Draw(drawLoc, flip ? base.Rotation.Opposite : base.Rotation, this);
+            }
+        }
+    }
+}
